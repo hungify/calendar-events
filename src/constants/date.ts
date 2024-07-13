c4d0ef35ef55e664087a989889d88eb87e4d010e
hungify <nmhungify@gmail.com>
2024-07-13 17:34:05 +0700

    feat: Update date constants and view options

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,24 +1,20 @@
-export const DEFAULT_MONTHS = [
-  { key: 0, title: 'January' },
-  { key: 1, title: 'February' },
-  { key: 2, title: 'March' },
-  { key: 3, title: 'April' },
-  { key: 4, title: 'May' },
-  { key: 5, title: 'June' },
-  { key: 6, title: 'July' },
-  { key: 7, title: 'August' },
-  { key: 8, title: 'September' },
-  { key: 9, title: 'October' },
-  { key: 10, title: 'November' },
-  { key: 11, title: 'December' },
-]
+import type { ViewOption } from '#/types/date'
+import { getDayNames, getMonthNames } from '#/utils/date'
 
-export const DEFAULT_DAYS = [
-  { key: 0, title: 'Sun' },
-  { key: 1, title: 'Mon' },
-  { key: 2, title: 'Tue' },
-  { key: 3, title: 'Wed' },
-  { key: 4, title: 'Thu' },
-  { key: 5, title: 'Fri' },
-  { key: 6, title: 'Sat' },
-]
+export const DEFAULT_MONTHS = getMonthNames()
+
+export const DEFAULT_DAYS = getDayNames()
+
+export const VIEW_OPTIONS = [{
+  id: 'day',
+  name: 'Day',
+}, {
+  id: 'week',
+  name: 'Week',
+}, {
+  id: 'month',
+  name: 'Month',
+}, {
+  id: 'year',
+  name: 'Year',
+}] as const satisfies ViewOption[]
