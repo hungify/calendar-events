f8a0d6bd8ec48d28ab66a55407fc284a5c951d7b
hungify <nmhungify@gmail.com>
2024-07-13 17:36:35 +0700

    feat: Add WeekGrid component

---
+++ src/components/WeekGrid.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { DEFAULT_DAYS } from '#/constants/date'
+import type { DayInfo } from '#/types/date'
+
+interface Props {
+  days: DayInfo[]
+}
+
+defineProps<Props>()
+
+function dayClass(dayIdx: number, daysLength: number) {
+  const classes = []
+  if (dayIdx === 0)
+    classes.push('rounded-tl-lg')
+  if (dayIdx === 6)
+    classes.push('rounded-tr-lg')
+  if (dayIdx === daysLength - 7)
+    classes.push('rounded-bl-lg')
+  if (dayIdx === daysLength - 1) {
+    classes.push('rounded-br-lg')
+  }
+
+  return classes.join(' ')
+}
+</script>
+
+<template>
+  <div class="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+    <div
+      v-for="day in DEFAULT_DAYS"
+      :key="day.id"
+    >
+      {{ day.narrow }}
+    </div>
+  </div>
+  <div class="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
+    <button
+      v-for="(day, dayIdx) in days" :key="day.format.LLLL" type="button"
+      class="py-1.5 hover:bg-gray-100 focus:z-10"
+      :class="[
+        day.isCurrentMonth ? 'bg-white text-gray-900' : 'bg-gray-50 text-gray-400',
+        dayClass(dayIdx, days.length),
+      ]"
+    >
+      <time
+        :datetime="String(day.format.LLLL)"
+        class="mx-auto flex h-7 w-7 items-center justify-center rounded-full"
+        :class="{
+          'bg-indigo-600 font-semibold text-white': day.isToday && day.isCurrentMonth,
+        }"
+      >
+        {{ day.format.D }}
+      </time>
+    </button>
+  </div>
+</template>
