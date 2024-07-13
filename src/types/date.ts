6d608f874da35f6d37c6883d9ac60dd3a29bd7f5
hungify <nmhungify@gmail.com>
2024-07-13 17:34:28 +0700

    feat: Add new interfaces for date and view options

--- src/types/date.ts
+++ src/types/date.ts
@@ -6,3 +6,38 @@ export interface DateType {
   month: number
   year: number
 }
+
+export interface ViewOption {
+  id: string
+  name: string
+}
+
+export interface EventInfo {
+  id: number
+  name: string
+  time: string
+  datetime: string
+  href: string
+}
+
+export interface DayInfo {
+  isCurrentMonth: boolean
+  isToday: boolean
+  isWeekend: boolean
+  format: {
+    ddd: string
+    dddd: string
+    D: string
+    DD: string
+    LLLL: string
+  }
+  events: EventInfo[]
+  isSelected: boolean
+}
+
+export interface MonthView {
+  name: string
+  days: DayInfo[]
+}
+
+export type YearView = MonthView[]
